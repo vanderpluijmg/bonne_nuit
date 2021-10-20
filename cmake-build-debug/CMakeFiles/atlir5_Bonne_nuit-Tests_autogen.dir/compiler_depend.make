adc8ce3162f9301e959c2dc8f760823399baf3a8
54786 <54786@etu.he2b.be>
2021-10-20 10:14:21 +0200

    remove

--- cmake-build-debug/CMakeFiles/atlir5_Bonne_nuit-Tests_autogen.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for atlir5_Bonne_nuit-Tests_autogen.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/CMakeFiles/atlir5_Bonne_nuit_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/atlir5_Bonne_nuit_autogen"
-  "atlir5_Bonne_nuit_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/atlir5_Bonne_nuit_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
