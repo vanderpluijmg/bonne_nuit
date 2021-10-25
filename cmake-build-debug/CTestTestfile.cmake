9c52566f171f7c4a3f427d708655b46f57f0d999
54786 <54786@etu.he2b.be>
2021-10-25 08:29:52 +0200

    gitignore

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gregory/Documents/2020-2021/Q5/ATLIR/C++/bonne_nuit
-# Build directory: /home/gregory/Documents/2020-2021/Q5/ATLIR/C++/bonne_nuit/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
