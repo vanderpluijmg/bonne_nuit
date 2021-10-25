dfaf06c1586dcde0ca5727714e2e0ea989266bd3
54786 <54786@etu.he2b.be>
2021-10-25 08:35:43 +0200

    gitignore final

--- cmake-build-debug/CMakeFiles/bonne_nuit_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/bonne_nuit_autogen"
-  "bonne_nuit_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/bonne_nuit_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
